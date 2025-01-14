e9374dbaea8b3d5dc498feb86bc712435455c55a
jwestreich <jwestreich1@gmail.com>
2025-01-13 23:33:52 -0500

    built autorunner to run scripts 1-4 with one execution

--- 00_parameters.R
+++ 00_parameters.R
@@ -19,6 +19,7 @@ city <- "DC"        #DC or NYC
 round_no <- 1
 google_sheet_link<-"https://docs.google.com/spreadsheets/d/188wl-XhzJ0fz0TCU9zHMOKmARJes0_Is-VX1b9NZgAs/edit?usp=sharing"
 pano<-"off" #on or off to save panorama pictures of locations
+auto_run<-"yes"
 
 at_large_percent<-1
 downtown_percent<-0
@@ -31,5 +32,12 @@ queens_percent<-.15
 bronx_percent<-.05
 subway_percent<-0
 
-code_base<-paste0("C:/Users/jwest/github/geoguessr_cities/",city)
+code_base<-paste0("C:/Users/jwest/github/geoguessr_cities/")
 output_location<-paste0("C:/Users/jwest/github/geoguessr_cities/",city,"/output/")
+
+if (auto_run=="yes"){
+  source(paste0(code_base,"01_borders.R"))
+  source(paste0(code_base,"02_locations.R"))
+  source(paste0(code_base,"03_metadata.R"))
+  source(paste0(code_base,"04_images.R"))
+}
